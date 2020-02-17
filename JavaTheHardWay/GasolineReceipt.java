4c17856cb04d285aa7050dd1288bfed82f0e491b
darinthompson <darint07@gmail.com>
2020-02-17 16:54:39 -0700

    study drills

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,19 @@
+public class GasolineReceipt {
+
+    public static void main(String[] args) {
+    
+        System.out.println("+------------------------+");
+        System.out.println("|                        |");
+        System.out.println("|      CORNER STORE      |");
+        System.out.println("|                        |");
+        System.out.println("| 2020-02-17  08:54AM    |");
+        System.out.println("|                        |");
+        System.out.println("|                        |");
+        System.out.println("| Gallons:       10.870  :");
+        System.out.println("| Price/gallon  $ 2.089  :");
+        System.out.println("|                        |");
+        System.out.println("| Fuel total:  $ 22.71   |");
+        System.out.println("|                        |");
+        System.out.println("+------------------------+");
+    }
+}
