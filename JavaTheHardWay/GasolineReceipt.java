7728121277cde3000430b90c432e904777a6912e
darinthompson <darint07@gmail.com>
2020-02-17 17:23:40 -0700

    Changed gas prices to a local price

--- JavaTheHardWay/GasolineReceipt.java
+++ JavaTheHardWay/GasolineReceipt.java
@@ -1,7 +1,7 @@
 public class GasolineReceipt {
 
     public static void main(String[] args) {
-    
+     
         System.out.println("+------------------------+");
         System.out.println("|                        |");
         System.out.println("|      CORNER STORE      |");
@@ -10,9 +10,9 @@ public class GasolineReceipt {
         System.out.println("|                        |");
         System.out.println("|                        |");
         System.out.println("| Gallons:       10.870  :");
-        System.out.println("| Price/gallon  $ 2.089  :");
+        System.out.println("| Price/gallon  $ 2.239  :");
         System.out.println("|                        |");
-        System.out.println("| Fuel total:  $ 22.71   |");
+        System.out.println("| Fuel total:  $ 24.34   |");
         System.out.println("|                        |");
         System.out.println("+------------------------+");
     }
