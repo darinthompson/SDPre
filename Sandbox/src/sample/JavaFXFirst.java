80c2a26e3d0657855feb4603b358c246be380e35
darinthompson <darint07@gmail.com>
2020-03-02 13:08:14 -0700

    added ability to 'shoot' from main object

--- Sandbox/src/sample/JavaFXFirst.java
+++ Sandbox/src/sample/JavaFXFirst.java
@@ -1,6 +1,6 @@
 package sample;
 
-import javafx.animation.TranslateTransition;
+import javafx.animation.*;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
@@ -16,16 +16,14 @@ import javafx.util.Duration;
 import java.util.Random;
 
 public class JavaFXFirst extends Application {
-    int viewWidth = 500;
-    int viewHeight = 500;
-    int initialXPos = viewWidth / 2; //100;
-    int initialYPos = viewWidth / 2;
-    int rectHeight = 30;
-    int rectWidth = 30;
-    int currentPosX = initialXPos;
-    int currentPosY = initialYPos;
-    int objectposX = 250;
-    int objectposY = 30;
+    private int viewWidth = 500;
+    private int initialXPos = viewWidth / 2; //100;
+    private int initialYPos = viewWidth / 2;
+    private int rectWidth = 30;
+    private int currentPosX = initialXPos;
+    private int currentPosY = initialYPos;
+    private int objectposX = 250;
+    private int objectposY = 30;
 
 
     @Override
@@ -35,12 +33,14 @@ public class JavaFXFirst extends Application {
 
         stage.setTitle("Hello World!");
         stage.setWidth(viewWidth);
+        int viewHeight = 500;
         stage.setHeight(viewHeight);
 
         VBox parent = new VBox();
         Scene scene1 = new Scene(parent);
 
         Rectangle rect = new Rectangle();
+        int rectHeight = 30;
         rect.setHeight(rectHeight);
         rect.setWidth(rectWidth);
         rect.setTranslateX(initialXPos);
@@ -54,6 +54,11 @@ public class JavaFXFirst extends Application {
         rect2.setHeight(rectHeight);
         rect2.setWidth(rectWidth);
 
+        Label l = new Label();
+
+        TranslateTransition tt = new TranslateTransition(Duration.millis(250), l);
+
+        tt.setCycleCount(1);
 
 
         scene1.setOnKeyPressed(new EventHandler<KeyEvent>() {
@@ -80,6 +85,14 @@ public class JavaFXFirst extends Application {
                         System.out.println("Going Right!");
                         rect.setTranslateX(currentPosX);
                         break;
+                    case SPACE:
+                        l.setText("*");
+                        tt.setFromX(currentPosX + 15);
+                        tt.setFromY(currentPosY  - 60);
+                        tt.setToY(currentPosY - viewHeight);
+                        tt.play();
+                        System.out.println("BOMBS AWAY!");
+                        break;
                 }
 
                 if(currentPosX == objectposX && currentPosY == objectposY) {
@@ -99,13 +112,13 @@ public class JavaFXFirst extends Application {
         });
 
 
-        parent.getChildren().addAll(rect, rect2);
+        parent.getChildren().addAll(rect, rect2, l);
         stage.setScene(scene1);
         stage.show();
 
     }
 
-    public int randNum() {
+    private int randNum() {
         int num;
         Random randy = new Random();
         do {
